db11f78bb0f2fa714e582b61d94f174c872f47d7
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2023-11-30 00:01:50 +0530

    Create cards section

--- src/components/status/Status.tsx
+++ src/components/status/Status.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './Status.scss';
+import status1 from '../../assets/images/card-1.png';
+import status2 from '../../assets/images/card-2.png';
+import status3 from '../../assets/images/card-3.png';
 
 const Status = () => {
+
   return (
-    <div>Status</div>
-  )
+    <div className='status-cards'>
+      <div className="status-cards-inner">
+        <img src={status1} alt="" className='status-img'/>
+        <img src={status3} alt="" className='status-img'/>
+        <img src={status2} alt="" className='status-img'/>
+      </div>
+    </div>
+  );
+
 }
 
 export default Status
