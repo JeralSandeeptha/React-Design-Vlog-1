3037dcee302894e81fb9e5a58b2e86908a0cc8de
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2023-11-29 17:46:26 +0530

    Create hero-down section

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Home.scss';
 import Hero from '../../components/hero/Hero';
+import HeroDown from '../../components/hero-down/HeroDown';
 
 const Home = () => {
 
   return (
     <>
       <Hero />
+      <HeroDown />
     </>
   );
   
