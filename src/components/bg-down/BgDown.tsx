32e9cc552f9998838889e0c4a6a620aee7b5bd2d
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2023-11-29 23:39:17 +0530

    Create down sections

--- src/components/bg-down/BgDown.tsx
+++ src/components/bg-down/BgDown.tsx
@@ -4,7 +4,12 @@ import './BgDown.scss';
 const BgDown = () => {
 
   return (
-    <div>BgDown</div>
+    <div className='bg-down'>
+      <div className="bg-down-inner">
+        <h5>The primary typeface for Tacos del Alma is "Cuuioolo" is a font that effortlessly marries with a touch of medernity. It's graceful curves and slightly rounded edges evoke a sense of handmade craftmanship, aligning perfectly with the artisanal nature or our tacos.</h5>
+        <h5>Complementing Cucciolo, the secondary typeface for Tacos del Alma is "Mytupi". Mytupi brings a modern and dynamic vibe to their brand. It's clean line and contempory aesthetics make it an excellent choice for secondary text providing a fresh contrasy while maintaining readability.</h5>
+      </div>
+    </div>
   );
 
 }
