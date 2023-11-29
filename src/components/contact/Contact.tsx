e21d67eb1a23ac9a0c4c9414971c18af070be4db
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2023-11-29 22:27:24 +0530

    Create Contact section

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import './Contact.scss';
 
 const Contact = () => {
+
   return (
-    <div>Contact</div>
-  )
+    <div className='contact'>
+      <div className="left">
+        <h4 className='job'>Designer</h4>
+        <div className="name">
+          <h4>Yuliia</h4>
+          <h4>Hrabynska</h4>
+        </div>
+        <div className="socials">
+          <div className="social">
+            <h5 className='parent'>INSTERGRAM:</h5>
+            <h5 className='child'>@julidesignco</h5>
+          </div>
+          <div className="social">
+            <h5 className='parent'>DRIBBLE:</h5>
+            <h5 className='child'>juliDesignCo</h5>
+          </div>
+          <div className="social">
+            <h5 className='parent'>PINTEREST:</h5>
+            <h5 className='child'>@julidesignco</h5>
+          </div>
+          <div className="social">
+            <h5 className='parent'>GMAIL:</h5>
+            <h5 className='child'>@juilahrabwork@gmail.com</h5>
+          </div>
+          <div className="social">
+            <h5 className='parent'>CREATIVE MARKET:</h5>
+            <h5 className='child'>juliDesignCo</h5>
+          </div>
+        </div>
+      </div>
+      <div className="right"></div>
+    </div>
+  );
+
 }
 
-export default Contact
+export default Contact;
