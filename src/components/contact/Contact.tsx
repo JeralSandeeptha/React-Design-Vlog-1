bb161e3b9cd846c2c2b92d587771e3b1a2976585
M.N. Jeral Sandeeptha <101165998+JeralSandeeptha@users.noreply.github.com>
2024-12-17 13:13:17 +0530

    Update Contact.tsx

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -8,29 +8,29 @@ const Contact = () => {
       <div className="left">
         <h4 className='job'>Designer</h4>
         <div className="name">
-          <h4>Yuliia</h4>
-          <h4>Hrabynska</h4>
+          <h4>Zyntra</h4>
+          <h4>Tech</h4>
         </div>
         <div className="socials">
           <div className="social">
             <h5 className='parent'>INSTERGRAM:</h5>
-            <h5 className='child'>@julidesignco</h5>
+            <h5 className='child'>@zyntratech</h5>
           </div>
           <div className="social">
             <h5 className='parent'>DRIBBLE:</h5>
-            <h5 className='child'>juliDesignCo</h5>
+            <h5 className='child'>zyntratech</h5>
           </div>
           <div className="social">
             <h5 className='parent'>PINTEREST:</h5>
-            <h5 className='child'>@julidesignco</h5>
+            <h5 className='child'>@zyntratech</h5>
           </div>
           <div className="social">
             <h5 className='parent'>GMAIL:</h5>
-            <h5 className='child'>@juilahrabwork@gmail.com</h5>
+            <h5 className='child'>@zyntra@gmail.com</h5>
           </div>
           <div className="social">
             <h5 className='parent'>CREATIVE MARKET:</h5>
-            <h5 className='child'>juliDesignCo</h5>
+            <h5 className='child'>zyntratech</h5>
           </div>
         </div>
       </div>
