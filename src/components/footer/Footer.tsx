635e74cc10d0a5ca740c730da3b9248294c678e9
M.N. Jeral Sandeeptha <101165998+JeralSandeeptha@users.noreply.github.com>
2024-12-17 12:59:01 +0530

    Update Footer.tsx

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import './Footer.scss';
 
 const Footer = () => {
   return (
-    <div className='footer'>This template was created by <br /> M.N. Jeral Sandeeptha</div>
+    <div className='footer'>This template was created by <br /> Zyntra Technologies</div>
   )
 }
 
